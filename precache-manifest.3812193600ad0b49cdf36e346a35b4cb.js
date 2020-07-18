4a0d93a59704550ebc113bdac453f450ccc183c5
Yeaseen <yeaseen.arafat96@gmail.com>
2020-07-18 23:36:18 +0600

    Updates

---
+++ precache-manifest.3812193600ad0b49cdf36e346a35b4cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b501b0486a0ed5fc6cafc59d6fa1d4eb",
+    "url": "/my-weather-app/index.html"
+  },
+  {
+    "revision": "65f4f86c8cbe72ede1ab",
+    "url": "/my-weather-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7180e0c313a93b7b1064",
+    "url": "/my-weather-app/static/css/main.27826fc9.chunk.css"
+  },
+  {
+    "revision": "65f4f86c8cbe72ede1ab",
+    "url": "/my-weather-app/static/js/2.48a145f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-weather-app/static/js/2.48a145f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7180e0c313a93b7b1064",
+    "url": "/my-weather-app/static/js/main.a7a3d01a.chunk.js"
+  },
+  {
+    "revision": "405ecb9917fff97d2277",
+    "url": "/my-weather-app/static/js/runtime-main.8b6e1a51.js"
+  }
+]);
